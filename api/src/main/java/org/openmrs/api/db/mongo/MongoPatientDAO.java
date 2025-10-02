b2c93b9314d000b96cf1e0e859bc53652a9d0ee8
Alexander Komyagin <adkomyagin@gmail.com>
2025-10-02 11:02:56 -0700

    chore(api): add MPL headers to MongoPatientDAO; remove annotations to rely on XML wiring

--- api/src/main/java/org/openmrs/api/db/mongo/MongoPatientDAO.java
+++ api/src/main/java/org/openmrs/api/db/mongo/MongoPatientDAO.java
@@ -1,17 +1,22 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
 package org.openmrs.api.db.mongo;
 
 import org.openmrs.*;
 import org.openmrs.api.db.DAOException;
 import org.openmrs.api.db.PatientDAO;
-import org.springframework.context.annotation.Profile;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.stereotype.Repository;
 
 import java.util.Collections;
 import java.util.List;
 
-@Repository("patientDAO")
-@Profile("mongo")
 public class MongoPatientDAO implements PatientDAO {
 
     private final MongoTemplate mongoTemplate;
